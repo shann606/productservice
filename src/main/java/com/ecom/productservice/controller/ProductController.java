b9d4aa11e475111d25c4a77be89eaa4acee957d0
shann606 <shann606@gmail.com>
2025-09-16 00:11:19 +0530

    added category post mapping

--- src/main/java/com/ecom/productservice/controller/ProductController.java
+++ src/main/java/com/ecom/productservice/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,14 @@ import lombok.extern.slf4j.Slf4j;
 public class ProductController {
 	
 	
-	@GetMapping("/{productId}")
-	private ResponseEntity addProduct(@PathVariable("productId") UUID productId)
-	{
-		System.out.println("printing product id "+ productId);
+	
+	
+	@PostMapping("/newproduct")
+	private ResponseEntity addProducts(){
+		
 		return null;
+		
+		
 	}
 
 }
