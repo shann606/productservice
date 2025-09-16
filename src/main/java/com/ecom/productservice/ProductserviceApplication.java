fa8e602cde23eec7185d106c6ecddf660b1ca2ad
shann606 <shann606@gmail.com>
2025-09-16 17:27:29 +0530

    added few more codes for spring jpa

--- src/main/java/com/ecom/productservice/ProductserviceApplication.java
+++ src/main/java/com/ecom/productservice/ProductserviceApplication.java
@@ -2,8 +2,10 @@ package com.ecom.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication 
+@EnableJpaRepositories(basePackages = "com.ecom.productservice.repository")
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
